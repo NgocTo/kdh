da2407c5e60600fd07dfe0109285dba8afbcb9a2
NgocTo <31940774+NgocTo@users.noreply.github.com>
2018-03-29 15:50:10 -0400

    added Jade's tables

---
+++ Database/Tables/Departments.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Departments]
+(
+	DepartmentId INT IDENTITY(1,1) CONSTRAINT departments_id_pk PRIMARY KEY,
+	DepartmentName VARCHAR(50) CONSTRAINT departments_departmentname_nn NOT NULL
+							   CONSTRAINT departments_departmentname_uk UNIQUE,
+	DepartmentDescription VARCHAR(max)
+)
