d63dcf381eff5b7dc723d1e63cf041b78fa08b43
gurinder <guribanwait07@gmail.com>
2018-03-30 14:34:27 -0400

    FAQ and donation database added

---
+++ Database/Tables/FAQ.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE FAQ
+(
+	QueId INT CONSTRAINT Faq_QueId_pk PRIMARY KEY IDENTITY(101,1),
+	Question VARCHAR(MAX) CONSTRAINT Faq_Question_nn NOT NULL
+						 CONSTRAINT Faq_Question_uk UNIQUE,
+	Answer VARCHAR(MAX),
+	DateCreated DateTime CONSTRAINT FAQ_DateCreated_df DEFAULT (getdate())
+						CONSTRAINT Faq_DateCreated_nn NOT NULL,
+	AuthorFirstName VARCHAR(50) CONSTRAINT Faq_AuthorFirstName_nn NOT NULL,
+	AuthorityFirstName VARCHAR(50) CONSTRAINT Faq_AuthorityFirstName_nn NOT NULL,
+	PurposeId VARCHAR(10) CONSTRAINT FAQ_PurposeId_nn NOT NULL,
+	CONSTRAINT [faq_AuthorFirstName_ck] CHECK (NOT [AuthorFirstName] like '%[^A-Za-z]%'),
+    CONSTRAINT [faq_AuthorityFirstName_ck] CHECK (NOT [AuthorityFirstName] like '%[^A-Za-z]%'),
+	CONSTRAINT FAQ_PurposeId_fk FOREIGN KEY (PurposeId) REFERENCES Purpose(PurposeId)
+);
---
+++ Database/Tables/Purpose.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Purpose
+(
+	PurposeId VARCHAR(10) CONSTRAINT Purpose_PurposeId_pk PRIMARY KEY ,
+	PurposeToCreate VARCHAR(MAX) CONSTRAINT Purpose_PurposeToCreate_nn NOT NULL
+	
+);
