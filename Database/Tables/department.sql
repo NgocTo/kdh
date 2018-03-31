1a5f05a0b4ec035295b9af0ae1ff989a14c54d0d
raj <rajchudasam@outlook.com>
2018-03-31 18:00:04 -0400

    tables added

---
+++ Database/Tables/department.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[departments]
+(
+	departmentid int IDENTITY,
+    department_name varchar(20) not null,
+    department_location varchar(10) not null,
+    department_description varchar(100) not null,
+    constraint pk_departmentid primary key(departmentid)
+)
---
+++ Database/Tables/doctors.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[doctors]
+(
+	doctorid int IDENTITY,
+    fname varchar(100) not null,
+    lname varchar(100) not null,
+    email varchar(100) not null,
+    address_line1 varchar(50) not null,
+    address_line2 varchar(50) not null,
+    postal_code varchar(7) not null,
+    mobile_no varchar(15) not null,
+    date_of_join datetime default getDate(),
+    departmentid int not null,
+    speciality varchar(20) not null,
+    province varchar(40) not null,
+    city varchar(40) not null,
+    constraint pk_doctorid primary key(doctorid),
+    constraint chk_fname check (fname not like '%[^A-Z]%'),
+    constraint chk_lname check (fname not like '%[^A-Z]%'),
+    constraint chk_email check (email LIKE '%___@___%'),
+    constraint chk_mobile check (mobile_no not like '%[^0-9]%'),
+    constraint chk_province check (province not like '%[^A-Z ]%'),
+    constraint chk_city check (city not like '%[^A-Z ]%')
+)
