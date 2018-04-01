6cedbcb0bc6964723423058102f2a044ae59cf96
gurinder <guribanwait07@gmail.com>
2018-03-31 23:29:19 -0400

    max size changed

--- Database/Tables/Purposes.sql
+++ Database/Tables/Purposes.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE Purposes
 (
 	PurposeId VARCHAR(10) CONSTRAINT Purposes_PurposeId_pk PRIMARY KEY ,
-	PurposeToCreate VARCHAR(MAX) CONSTRAINT Purposes_PurposeToCreate_nn NOT NULL
+	PurposeToCreate VARCHAR(900) CONSTRAINT Purposes_PurposeToCreate_nn NOT NULL
 	
 );
