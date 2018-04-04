1d6134ce547d1d252b4ca5a05c672d0737e26779
Terumi Kusaka <t.kusaka3@gmail.com>
2018-04-03 22:08:21 -0400

    modified Create and AccountRegistration action according to the new model structures

--- kdh/Utils/DateValidator.cs
+++ kdh/Utils/DateValidator.cs
@@ -8,6 +8,13 @@ namespace kdh.Utils
 {
     public class DateValidator : ValidationAttribute
     {
+        /// <summary>
+        /// Validate if the date passed to the paramater is day before today (past date). 
+        /// Return true if the passed date is past or null.
+        /// Return false if the passed date is future.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
         public override bool IsValid(object date)
         {
 
