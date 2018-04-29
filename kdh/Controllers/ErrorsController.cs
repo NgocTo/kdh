9d38d174bfa5dfe5ceace4ef0aa9a6d8092aeb1e
NgocTo <31940774+NgocTo@users.noreply.github.com>
2018-04-29 16:41:37 -0400

    added config for page not found

---
+++ kdh/Controllers/ErrorsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kdh.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult Index()
+        {
+            return View("~/Views/Errors/Details.cshtml");
+        }
+
+        public ActionResult Code(int? id)
+        {
+            //By default, if you want to use a number parameter you have to use "id" as the variable name
+            //If you want to use an optional paramter, you include the "?" beside the datatype. 
+
+            switch (id)
+            {
+                //[domain]/errors/code/404
+                case 404:
+                    return View("~/Views/Errors/NotFound.cshtml");
+                default:
+                    return View("~/Views/Errors/Details.cshtml");
+            }
+        }
+    }
+}
